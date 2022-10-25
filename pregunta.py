2bd9c8427099fe677076669c0e550b29cb67ab5f
adcalderonc <112212138+adcalderonc@users.noreply.github.com>
2022-10-24 23:18:10 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -13,8 +13,21 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+
+    df['sexo']=df.sexo.str.lower()
+    df['tipo_de_emprendimiento']=df.tipo_de_emprendimiento.str.lower()
+    df['idea_negocio']=df.idea_negocio.str.lower().str.replace(' ', '_').str.replace('-', '_')
+    df['barrio']=df.barrio.str.lower().str.replace(' ', '_').str.replace('-', '_').str.replace('.', '')
+    df['monto_del_credito']=df.monto_del_credito.str.replace('$', '').str.replace(',', '')
+    df['monto_del_credito']=df.monto_del_credito.map(lambda x: x.replace('.00', '') if re.search(r".\d{2}$", x) else x)
+    df['línea_credito']=df.línea_credito.str.lower().str.replace(' ', '_').str.replace('-', '_')
+
+    df.dropna(subset=['sexo'], inplace= True)
+    df.dropna(subset=['tipo_de_emprendimiento'], inplace= True)
+    df.dropna(subset=['idea_negocio'], inplace= True)
+    df.dropna(subset=['barrio'], inplace= True)
+    df.dropna(subset=['monto_del_credito'], inplace= True)
+    df.dropna(subset=['línea_credito'], inplace= True)
+    
 
     return df
